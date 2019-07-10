c20eff8a84e11584fc0bb092e7f2ea0b6367b60c
Aaron Wilson <codingsensei19@gmail.com>
2019-07-10 20:55:27 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,8 +3,7 @@ puts"Hi, my name is #{name}."
 end
 
 def #introduction_with_language
-
-endname = "Aaron"
+name = "Aaron"
 language = "Ruby"
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
