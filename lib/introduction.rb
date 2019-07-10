b082bf5e34dcaed96d0bbb7792a7513dcd0b5cde
Aaron Wilson <codingsensei19@gmail.com>
2019-07-10 20:46:41 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,5 +5,5 @@ end
 name = Aaron
 language = software
 def
-  puts "Hi, my name is #{Aaron} and I am learning to program in #{language}."
+  puts"Hi, my name is #{Aaron} and I am learning to program in #{language}."
 end
